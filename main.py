edf71ff5191890e31044cedca6a70519813724f3
Kaveesha Himasanka <kaveee143@gmail.com>
2023-02-25 14:19:40 +0530

    json object

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world!")
+student = {
+    "name": "Kaveesha",
+    "age": "22",
+    "address": {
+        "country": "Sri Lanka",
+        "town": "Negombo"
+    }
+}
+
+print(student["name"])
+print(type(student))
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 21, 32, 25]
+print(a)
+print(type(a))
+
